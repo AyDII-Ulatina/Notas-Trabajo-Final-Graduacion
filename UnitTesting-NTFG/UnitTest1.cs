0abec3c6b41df2c67214d4f69ed6c9081e67d024
BrandonZS <124947206+BrandonZS@users.noreply.github.com>
2024-10-29 18:21:26 -0600

    Structured: Add structure to Unit test in Develop

---
+++ UnitTesting-NTFG/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace UnitTesting_NTFG
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
