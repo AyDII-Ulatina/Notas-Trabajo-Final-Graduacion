f3dcd1ea26b47fe184671a1a4c7af00c71027fe4
BrandonZS <abrandon34zs@gmail.com>
2024-11-03 23:54:50 -0600

    Add Proyecto Model

---
+++ WPF_NTFG/Modelos/Proyecto.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_NTFG.Modelos
+{
+    public class Proyecto
+    {
+        public int idProyecto { get; set;}
+        public string nomProyecto { get; set;}
+        public DateTime fecha { get; set;}
+        public string nomTutor { get; set;}
+        public string nomMetodologo { get; set;}
+        public int idEstado { get; set;}
+        public string nomEstudiante { get; set;}
+
+    }
+}
