56eecec9ac63fc39bcc8c942dccb573b5611e1ca
BrandonZS <abrandon34zs@gmail.com>
2024-11-03 23:57:37 -0600

    Add Profesor Model

---
+++ WPF_NTFG/Modelos/Profesor.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_NTFG.Modelos
+{
+    public class Profesor
+    {
+        public int idProfesor {  get; set; }
+        public string nomProfesor { get; set; }
+        public bool rolTutor { get; set; }
+        public bool rolMetodologo { get; set; }
+    }
+}
